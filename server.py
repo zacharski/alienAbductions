301b15a301cfe933f6f9e4700cfaf61fa2548e3f
Ron Zacharski <ron.zacharski@gmail.com>
2014-01-27 16:50:41 +0000

    added simple examples of forms

--- server.py
+++ server.py
@@ -15,6 +15,25 @@ def report2():
                'lastname': request.form['lastname']}
   return render_template('report2.html', abduction = abduction)
 
+@app.route('/simple')
+def simple():
+  return render_template('simple.html')
+
+@app.route('/simple2', methods=['POST'])
+def simple2():
+  return render_template('simple2.html')
+
+
+@app.route('/simple3')
+def simple3():
+  return render_template('simple3.html')
+
+@app.route('/simple4', methods=['POST'])
+def simple4():
+  return render_template('simple4.html', name=request.form['name'])
+
+
+
 if __name__ == '__main__':
     app.debug=True
     app.run(host='0.0.0.0', port=3000)
