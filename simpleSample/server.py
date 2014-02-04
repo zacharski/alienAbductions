5b127ef475f3bfcba5e4bfcd59d4f6bd66d3fd34
Ron Zacharski <ron.zacharski@gmail.com>
2014-02-04 02:42:34 +0000

    Added Dictionary Cursor Example

--- simpleSample/server.py
+++ simpleSample/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import MySQLdb
 import utils
 
 app = Flask(__name__)
@@ -21,6 +22,23 @@ def mainIndex():
     rows = cur.fetchall()
 
     return render_template('index.html', posts=rows)
+
+@app.route('/cursor', methods=['GET', 'POST'])
+def cursorDictDemo():
+    db = utils.db_connect()
+    cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+    
+    # if user typed in a post ...
+    if request.method == 'POST':
+        query = "INSERT INTO posts VALUES ('" + request.form['name'] + "', '" + request.form['msg'] + "')"
+        # Print query to console (useful for debugging)
+        print query
+        cur.execute(query)
+
+    cur.execute('select * from posts')
+    rows = cur.fetchall()
+
+    return render_template('index2.html', posts=rows)
 
 
 
